37c8f231da6e9ee26fb4abd7038009c1059315ba
xixex <mikhailsontsav@gmail.com>
2018-09-11 15:05:08 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -1,31 +1,45 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this._storage = [];
+        this._comparator = (a, b) => {
+            return a - b;
+        }
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        this._storage.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this._storage[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this._storage.length;
+    }
 
-  toArray() {
-    // your implementation
-  }
+    toArray() {
+        return this._storage;
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    sort(indices) {
+        let tempArray = [];
+        indices.sort((a, b) => {
+            return a - b;
+        });
+        indices.map(el => {
+            tempArray.push(this._storage[el]);
+        });
+        tempArray.sort(this._comparator);
+        for (let i = 0; i < indices.length; i++) {
+            let index = indices[i];
+            this._storage[index] = tempArray[i];
+        }
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    setComparator(compareFunction) {
+        this._comparator = compareFunction;
+    }
 }
 
 module.exports = Sorter;
